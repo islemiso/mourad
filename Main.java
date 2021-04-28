9c4fcc84dd66aabd23fa689a4040dddc303f5381
islemiso <82879480+islemiso@users.noreply.github.com>
2021-04-28 09:57:06 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
-  classe  publique Main {
-    public  static  void  main ( String [] args ) {
-        Système . dehors . println ( " Bonjour, Xavki! " );
-    }
+    classe publique Main {  
+      public  static  void  main ( String [] args ) {
+        Système . dehors . println ( " Bonjour, Xavki! " );   
+    } 
 }
 
