5cc6ae76e811f0d8216b6ff2ce605a6101e30544
islam iso <alaouache-islam@hotmail.fr>
2021-04-29 02:07:28 -0700

    modification de Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
-    classe publique Main {  
+    public class Main {  
       public  static  void  main ( String [] args ) {
-        Système . dehors . println ( " Bonjour, Xavki! " );   
+        Système . dehors . println ( " Bonjour, islemiso! " );   
     } 
 }
 
